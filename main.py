694636d437037307055808234a537a2a38b0f099
Lucas Alves <lucasr2002@gmail.com>
2022-06-13 16:38:18 -0300

    further text fine reading

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 # open input file
-input = open("/home/prrg/TC/input.txt", "r")
+input = open("/home/prrg/TC/simuladorAFN/input.txt", "r")
 afn = {}
 
-str = input.readline()
-if "alfabeto=" in str:
-    cleanStr = str.replace("alfabeto=","")
-    cleanStr = cleanStr.replace(" ","")
-    afn["alfabeto"] = cleanStr.split(",")
-    print(afn["alfabeto"])
+while (True):
+    str = input.readline()
+    if "alfabeto=" in str:
+        cleanStr = str.replace("alfabeto=","")
+        cleanStr = cleanStr.replace("\n","")
+        cleanStr = cleanStr.replace(" ","")
+        afn["alfabeto"] = cleanStr.split(",")
+        print(afn["alfabeto"])
+    if "estados=" in str:
+        cleanStr = str.replace("estados=","")
+        cleanStr = cleanStr.replace("\n","")
+        afn["estados"] = cleanStr.split(",")
+        print(afn["estados"])
+    if "inicial=" in str:
+        cleanStr = str.replace("inicial=","")
+        cleanStr = cleanStr.replace("\n","")
+        cleanStr = cleanStr.replace(" ","")
+        afn["inicial"] = cleanStr
+        print(afn["inicial"])
+    if "finais=" in str:
+        cleanStr = str.replace("finais=","")
+        cleanStr = cleanStr.replace("\n","")
+        cleanStr = cleanStr.replace(" ","")
+        afn["finais"] = cleanStr.split(",")
+        print(afn["finais"])
+    if not str:
+        break
