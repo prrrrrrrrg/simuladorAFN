355b1fa320b212cdaf72f79c58ae73f629e96323
Lucas Alves <lucasr2002@gmail.com>
2022-06-14 19:57:16 -0300

    added transitions, shortened earlier code, added close function

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
-# open input file
+#open input file
 input = open("/home/prrg/TC/simuladorAFN/input.txt", "r")
 afn = {}
 
+#string cleanup
+def strCleanup(str):
+    str = str.replace("\n","")
+    str = str.replace(" ","")
+    return str.split(",")
+
+#read input file
 while (True):
-    str = input.readline()
-    if "alfabeto=" in str:
-        cleanStr = str.replace("alfabeto=","")
-        cleanStr = cleanStr.replace("\n","")
-        cleanStr = cleanStr.replace(" ","")
-        afn["alfabeto"] = cleanStr.split(",")
+    str = input.readline() #read line
+    if "alfabeto=" in str: #save alphabet
+        str = str.replace("alfabeto=","")
+        afn["alfabeto"] = strCleanup(str)
         print(afn["alfabeto"])
-    if "estados=" in str:
-        cleanStr = str.replace("estados=","")
-        cleanStr = cleanStr.replace("\n","")
-        afn["estados"] = cleanStr.split(",")
+    if "estados=" in str: #save states
+        str = str.replace("estados=","")
+        afn["estados"] = strCleanup(str)
         print(afn["estados"])
-    if "inicial=" in str:
-        cleanStr = str.replace("inicial=","")
-        cleanStr = cleanStr.replace("\n","")
-        cleanStr = cleanStr.replace(" ","")
-        afn["inicial"] = cleanStr
+    if "inicial=" in str: #save initial state
+        str = str.replace("inicial=","")
+        afn["inicial"] = strCleanup(str)
         print(afn["inicial"])
-    if "finais=" in str:
-        cleanStr = str.replace("finais=","")
-        cleanStr = cleanStr.replace("\n","")
-        cleanStr = cleanStr.replace(" ","")
-        afn["finais"] = cleanStr.split(",")
+    if "finais=" in str: #save final state
+        str = str.replace("finais=","")
+        afn["finais"] = strCleanup(str)
         print(afn["finais"])
+    if "transicoes" in str: #save transitions
+        afn["transicoes"] = {} #create dict entry for transitions
+        afn["transicoes"]["epsilon"] = {} #create nested dict entry for empty transitions
+        for i in afn["alfabeto"]: #create nested dict entry for each alphabet entry
+            afn["transicoes"][i] = {}
+        while (True): #read all the transition entries
+            str = input.readline() #move to next transition
+            if not str: #if no transitions left, stop
+                break
+            str = strCleanup(str) #get transition values
+            if not str[0] in afn["transicoes"][str[2]]: #check if an entry already exists for jump-off state
+                afn["transicoes"][str[2]][str[0]] = [] 
+            if not str[1] in afn["transicoes"][str[2]][str[0]]: #check if duplicate transition
+                afn["transicoes"][str[2]][str[0]].append(str[1])
+        print(afn["transicoes"])            
     if not str:
         break
+    
+print(afn)
+
+input.close()
